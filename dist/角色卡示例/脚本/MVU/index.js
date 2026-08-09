a417c30b2eb059138ef87a887bfc5048cbd564c2
Vin <vincentrong20050210@gmail.com>
2026-08-09 15:49:49 +0200

    Fix shop robbery post-victory prompt

---
+++ dist/角色卡示例/脚本/MVU/index.js
@@ -0,0 +1,11 @@
+import * as __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_artifact_bundle_js_63c1ca07__ from "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js";
+
+var __webpack_modules__ = {
+  "./示例/角色卡示例/脚本/MVU/index.ts"() {
+    eval('{\n;// external "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js"\nconst bundle_js_namespaceObject = __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_artifact_bundle_js_63c1ca07__;\n;// ./示例/角色卡示例/脚本/MVU/index.ts\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi/npLrkvosv6KeS6Imy5Y2h56S65L6LL+iEmuacrC9NVlUvaW5kZXgudHMiLCJtYXBwaW5ncyI6Ijs7QUFBQSxNQUFNLHlCQUE0Qix1SDs7QUNBd0QiLCJzb3VyY2VzIjpbInNyYzovL3RhdmVybl9oZWxwZXJfdGVtcGxhdGUvZXh0ZXJuYWwgbW9kdWxlIFwiaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZS9hcnRpZmFjdC9idW5kbGUuanNcIiIsInNyYzovL3RhdmVybl9oZWxwZXJfdGVtcGxhdGUv56S65L6LL+inkuiJsuWNoeekuuS+iy/ohJrmnKwvTVZVL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IF9fV0VCUEFDS19OQU1FU1BBQ0VfT0JKRUNUX18gPSBfX1dFQlBBQ0tfRVhURVJOQUxfTU9EVUxFX2h0dHBzX3Rlc3RpbmdjZl9qc2RlbGl2cl9uZXRfZ2hfTWFnaWNhbEFzdHJvZ3lfTWFnVmFyVXBkYXRlX2FydGlmYWN0X2J1bmRsZV9qc182M2MxY2EwN19fOyIsImltcG9ydCAnaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZS9hcnRpZmFjdC9idW5kbGUuanMnO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./示例/角色卡示例/脚本/MVU/index.ts\n\n}');
+  }
+};
+
+let __webpack_exports__ = {};
+
+__webpack_modules__["./示例/角色卡示例/脚本/MVU/index.ts"]();
